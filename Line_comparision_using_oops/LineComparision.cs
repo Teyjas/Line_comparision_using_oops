be0b51fa2edde0dc3c38e91c3de30b16701fa8a8
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-02 18:33:50 +0530

    Add project files.

---
+++ Line_comparision_using_oops/LineComparision.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Using_Opps
+{
+    internal class Line
+    {
+        public double GetLength()
+        {
+            Console.WriteLine("Enter value of points (x1,y1,x2,y2)");
+
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
+    }
+}
---
+++ Line_comparision_using_oops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Line_Comparison_Using_Opps
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to line comparison program");
+
+            Line line1 = new Line();
+            Line line2 = new Line();
+            double length1 = line1.GetLength();
+            double length2 = line2.GetLength();
+            int val = length1.CompareTo(length2);
+
+            if (val == 0)
+                Console.WriteLine("Length of lines are equal");
+            else if (val > 0)
+                Console.WriteLine("Length of line 1 is greater than line 2");
+            else
+                Console.WriteLine("Length of line 2 is greater than line 1");
+        }
+    }
+}
